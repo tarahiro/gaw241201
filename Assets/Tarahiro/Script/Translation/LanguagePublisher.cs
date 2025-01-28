a7f9942d1bae823e904df8774b0ccd9af18d0031
tarahiro <38371023+tarahiro@users.noreply.github.com>
2025-01-28 12:45:16 +0900

    MessagePipe追加

---
+++ Assets/Tarahiro/Script/Translation/LanguagePublisher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tarahiro;
+using UniRx;
+using UnityEngine;
+using VContainer;
+
+namespace Tarahiro
+{
+    public class LanguagePublisher
+    {
+    }
+}
