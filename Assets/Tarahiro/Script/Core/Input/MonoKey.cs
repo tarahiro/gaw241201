dc04f5fedc76bad28e1f72c64f65a6784304bbf8
tarahiro <38371023+tarahiro@users.noreply.github.com>
2025-03-20 09:51:20 +0900

    対応

--- Assets/Tarahiro/Script/Core/Input/MonoKey.cs
+++ Assets/Tarahiro/Script/Core/Input/MonoKey.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Tarahiro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static PlasticPipe.PlasticProtocol.Messages.NegotiationCommand;
 
 namespace Tarahiro.TInput
 {
